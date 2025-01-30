05b46a291d42749b0b02c6ec62cf2946ae31ed25
Alin Voia <voia.alin@yahoo.com>
2025-01-30 02:34:07 +0200

    Vers_1
    This is the first version of the course

---
+++ story_content/6HvooG23sAP_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.95.33670.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:04.415%0D%0ALearning%20objectives%20are%20the%20things%20that%20you%20might%20learn%20and%20understand%20after%20completing%20the%20course.%0D%0A%0D%0A"}')
